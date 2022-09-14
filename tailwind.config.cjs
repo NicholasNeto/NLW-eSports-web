24e38efcd60414b03b5aefa9af2c5cccde3281b0
NicholasNeto <nicholasneto@outlook.com>
2022-09-13 23:29:44 -0300

    nlw-web add library

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,9 @@ module.exports = {
   ],
   theme: {
     extend: {
-      // colors: {},
+      fontFamily: {
+        sans: ['Inter', 'sans-serif']
+      },
       backgroundImage: {
         galaxy: "url('./background-galaxy.png')",
         'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 27.08%, #43E7AD 33.94%, #E1D55D 40.57%)',
