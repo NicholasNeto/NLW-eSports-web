a23a7455b3fb935e1d41015bc1b7aeb814f3c7b5
NicholasNeto <nicholasneto@outlook.com>
2022-09-13 22:07:27 -0300

    nlw-web add box with fake imgs

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,14 @@ function App() {
 
     <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
       <img src={logoImg} alt="Logo eSports" />
-      {/* <h1 className='text-6xl text-white font-black mt-20'>
+      <h1 className='text-6xl text-white font-black mt-20'>
         Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> está aqui.
       </h1>
       <div className='grid grid-cols-6 gap-6'>
         <a href="">
           <img src="/game-1.png" alt="" />
         </a>
-      </div> */}
+      </div>
     </div>
   )
 }
